c86d77b3c228ddc86f32e9ad548c2df54f8f7dd2
Francisco Casillas <chiccasillas@gmail.com>
2026-01-28 17:30:46 -0600

    Initial Commit

---
+++ CoupleBudget/CoupleBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoupleBudgetApp.swift
+//  CoupleBudget
+//
+//  Created by Francisco Jean on 28/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CoupleBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoupleBudgetTests/CoupleBudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoupleBudgetTests.swift
+//  CoupleBudgetTests
+//
+//  Created by Francisco Jean on 28/01/26.
+//
+
+import Testing
+@testable import CoupleBudget
+
+struct CoupleBudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
