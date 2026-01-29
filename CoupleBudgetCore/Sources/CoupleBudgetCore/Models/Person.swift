dd1bf40da578fd5b551feb03a00ec5f121d449ed
Francisco Casillas <chiccasillas@gmail.com>
2026-01-29 13:54:33 -0600

    feat: add Person model

---
+++ CoupleBudgetCore/Sources/CoupleBudgetCore/Models/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  CoupleBudgetCore
+//
+//  Created by Francisco Jean on 29/01/26.
+//
+
+import Foundation
+public struct Person {
+    public let id: UUID
+    public var name: String
+    public var isPlanifiable: Bool
+
+    public init(id: UUID = UUID(), name: String, isPlanifiable: Bool) {
+        self.id = id
+        self.name = name
+        self.isPlanifiable = isPlanifiable
+    }
+}
+
